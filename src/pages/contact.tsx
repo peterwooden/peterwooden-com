79135d7515816b8ad807c54d879a9d2f00931d61
Peter Wooden <peter.r.wooden@gmail.com>
2021-02-04 22:13:43 +1100

    updated projects and contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -11,44 +11,17 @@ type DataProps = {
   }
 }
 
-const UsingTypescript: React.FC<PageProps<DataProps>> = ({
+const Contact: React.FC<PageProps<DataProps>> = ({
   data,
   path,
   location,
 }) => (
-  <Layout title="Using TypeScript" location={location}>
-    <SEO title="Using TypeScript" />
-    <h1>Gatsby supports TypeScript by default!</h1>
+  <Layout title="Contact" location={location}>
+    <SEO title="Contact" />
     <p>
-      This means that you can create and write <em>.ts/.tsx</em> files for your
-      pages, components etc. Please note that the <em>gatsby-*.js</em> files
-      (like gatsby-node.js) currently don't support TypeScript yet.
+      Please contact me on LinkedIn.
     </p>
-    <p>
-      For type checking you'll want to install <em>typescript</em> via npm and
-      run <em>tsc --init</em> to create a <em>.tsconfig</em> file.
-    </p>
-    <p>
-      You're currently on the page "{path}" which was built on{" "}
-      {data.site.buildTime}.
-    </p>
-    <p>
-      To learn more, head over to our{" "}
-      <a href="https://www.gatsbyjs.com/docs/typescript/">
-        documentation about TypeScript
-      </a>
-      .
-    </p>
-    <Link to="/">Go back to the homepage</Link>
   </Layout>
-)
+);
 
-export default UsingTypescript
-
-export const query = graphql`
-  {
-    site {
-      buildTime(formatString: "YYYY-MM-DD hh:mm a z")
-    }
-  }
-`
+export default Contact;
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const Projects = ({
+  location,
+}) => (
+  <Layout title="Projects" location={location}>
+    <SEO title="Projects" />
+    <div className="flex flex-col gap-4">
+      <div>
+        <p className="text-lg font-bold">Modern Logo Designer (2021)</p>
+        <p>A satirical app to design modern logos... which all look the same.</p>
+        <Link to="/projects/modern-logo-designer" className="text-blue-600 hover:underline">Link</Link>
+      </div>
+      <div>
+        <p className="text-lg font-bold">Polymer Documents (2020)</p>
+        <p>A full stack SAAS app for creating dynamic JSONSchema forms and automating complex legal documents</p>
+        <ul className="list-disc list-inside">
+          <li className="m-0">React/Redux/React Router SPA</li>
+          <li className="m-0">Rust GraphQL backend plus Nodejs services</li>
+          <li className="m-0">Postgres database</li>
+          <li className="m-0">CI/CD pipeline using AWS and Github Actions</li>
+        </ul>
+        <a href="https://polymerlaw.com" className="text-blue-600 hover:underline">Link</a>
+      </div>
+      <div>
+        <p className="text-lg font-bold">Annotate Bounding Boxes (2020)</p>
+        <p>A tool to assist OCR template generation. Drop in an image, and use your cursor to draw boxes around text areas. The corresponding coordinates will be provided.</p>
+        <Link to="/projects/annotate-bounding-boxes" className="text-blue-600 hover:underline">Link</Link>
+      </div>
+      <div>
+        <p className="text-lg font-bold">Legal advice chatbot (2017)</p>
+        <p>Expert system to advice on Tort law matters. Made as a way to revise (procrastinate) for my studies.</p>
+      </div>
+      <p>
+        See my <a href="https://github.com/peterwooden">GitHub</a>.
+      </p>
+
+    </div>
+  </Layout>
+)
+
+export default Projects
