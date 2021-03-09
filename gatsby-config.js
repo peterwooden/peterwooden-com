c5007d6e4b2034b09fe37e293550661d56ac8060
Peter Wooden <peter.r.wooden@gmail.com>
2021-03-09 13:37:22 +0000

    intall google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,6 +98,15 @@ module.exports = {
         siteUrl: "https://peterwooden.com",
       }
     },
-    'gatsby-plugin-postcss'
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-RJWGR86QTF", // Google Analytics / GA
+        ],
+      },
+    },
   ],
 }
